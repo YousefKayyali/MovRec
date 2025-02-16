1247e9d45e567fb9cd5f70cebce7163e244a2a5e
YousefKayyali <yousef.al.alkayyali@gmail.com>
2025-02-16 15:26:58 +0300

    modify alg of add rating with user id  0 and make modification on user to user to calculate person correlation

--- data manipulation/addnewmovieratingwithuserId0.py
+++ data manipulation/addnewmovieratingwithuserId0.py
@@ -1,16 +1,19 @@
 import pandas as pd
-destination = r"D:\project data\rating data\update_4\rating_4.csv"
-source = r"D:\project data\rating data\update_3\rating_3.csv"
+destination = r"D:\movrec\update_4\rating_4.csv"
+source = r"D:\movrec\update_3\rating_3.csv"
 data = pd.read_csv(source)
 print( len(data))
 movie = {}
 new_rows = [] 
 size = len(data)
 for i in range(size):
-    movie_name = data.iloc[i, 1]
-    if movie_name not in movie:
-        new_rows.append([0, movie_name, 2.5])
-        movie[movie_name] = True
+    movie_id= data.iloc[i, 1]
+    if movie_id not in movie:
+        new_rows.append([-3, movie_id, 2.5])
+        new_rows.append([0, movie_id, 2.5])
+        movie[movie_id] = True
+        if len(movie.keys())==7616:
+            break
 if new_rows:
     new_data = pd.DataFrame(new_rows, columns=[data.columns[0], data.columns[1], 'rating'])
     data = pd.concat([data, new_data], ignore_index=True)
