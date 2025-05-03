ba4bb14efadee76a30b1929f4ee76ae27fb7b422
YousefKayyali <yousef.al.alkayyali@gmail.com>
2025-05-03 17:21:04 +0300

    Delete MovRec-master directory

--- MovRec-master/Models/ViewModels/CategorySelectionViewModel.cs
+++
@@ -1,7 +0,0 @@
-using MovRec.Models;
-
-public class CategorySelectionViewModel
-{
-    public List<Genre> AvailableGenres { get; set; }
-    public List<string> SelectedGenres { get; set; } = new List<string>();
-}
--- MovRec-master/Models/ViewModels/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MovRec.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
